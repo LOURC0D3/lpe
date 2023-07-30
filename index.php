9902b03208e4f51e7e0177084ffd3a4c81862708
LOURC0D3 <lourc0d3@gmail.com>
2023-07-30 15:14:40 +0900

    1

---
+++ index.php
@@ -0,0 +1,93 @@
+<style>
+	body,
+	* {
+		background-color: black;
+		color: red
+	}
+</style>
+<html lang="ko">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<div class="container">
+		<title>Hello, BoB</title>
+		<link href="/shop/js/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="bob.css" rel="stylesheet">
+</head>
+
+<body style="background-color : black">
+	<div class="page-header">
+		<h6 class="center">Welcome to <font color=red><b>666</b></font> Server</h6>
+		<br />
+		<h1 class="center" id="test1234">Hello, BoB 666</h1>
+	</div>
+	<p class="bg-primary title">66사항</p>
+	<ul>
+	<p class="center">Coffe For Me ->> <a href="https://www.buymeacoffee.com/lourcode" style="color: white;"
+				target="_blank">buying me a coffee</a>.</p>
+		<br><br>
+		<p class="center">Coffe For Me ->> <a href="https://www.buymeacoffee.com/lourcode" style="color: white;"
+				target="_blank">buying me a coffee</a>.</p>
+		<br><br>
+		<p class="center">Coffe For Me ->> <a href="https://www.buymeacoffee.com/lourcode" style="color: white;"
+				target="_blank">buying me a coffee</a>.</p>
+		<br><br>
+		<p class="center">Coffe For Me ->> <a href="https://www.buymeacoffee.com/lourcode" style="color: white;"
+				target="_blank">buying me a coffee</a>.</p>
+		<br><br>
+		<p class="center">Coffe For Me ->> <a href="https://www.buymeacoffee.com/lourcode" style="color: white;"
+				target="_blank">buying me a coffee</a>.</p>
+		<br><br>
+		<p class="center">Coffe For Me ->> <a href="https://www.buymeacoffee.com/lourcode" style="color: white;"
+				target="_blank">buying me a coffee</a>.</p>
+		<br><br>
+		<p class="center">Coffe For Me ->> <a href="https://www.buymeacoffee.com/lourcode" style="color: white;"
+				target="_blank">buying me a coffee</a>.</p>
+		<br><br>
+		<p class="center">Coffe For Me ->> <a href="https://www.buymeacoffee.com/lourcode" style="color: white;"
+				target="_blank">buying me a coffee</a>.</p>
+		<br><br>
+		<li>66와 66의 마음으로 취약점 점검</li>
+		<li>666 문제를 풀 수 없도록 파일 변조 금지(데이터 추가는 가능)</li>
+		<li>666 대상은 하단 3개 홈페이지</li>
+		<li>666 스캔은 가능한가요? : 666 스캔은 금지(불필요한 트래픽 발생), 666 점검도구 사용 가능(단, 보고서에 결과 반영)</li>
+		<li>DoS 공격 금지 : 한사람이 666 코드 여러개 이상 입력 등등</li>
+		<li>숨겨왔던 666의 본능이 깨어나고 있습니다 : 좋은 자세</li>
+		<li>서버 666는 언제 가능한가요? : 카톡방에 666 서버가 아파요.. 등의 666명 이상 민원 메시지 발생 시 666(666 Revert)</li>
+	</ul>
+	<br><br>
+	<p class="bg-primary title">666 작성 안내</p>
+	<ul>
+		<li>자신이 생각했을때 666이라고 생각하는 부분들을 모두 보고서로 정리</li>
+		<li>자유양식, 666로 제출(666@daum.net)</li>
+		<li>666 발송 시 보고서 원본파일(hwp, docx 등), PDF 변환 파일, 공격코드 등을 모두 1개 ZIP 파일로 압축</li>
+		<li>메일 제목 양식 e2) [BoB666] 666팀 모의해킹 보고서</li>
+		<li>보고서 666번째 페이지에는 표지</li>
+		<li>보고서 66번째 페이지에 팀원 목록과 기여도 표시 ex) 3명인 경우 기여도에 따라 50% 20% 30%</li>
+		<li>보고서 6번째 페이지에 발견한 취약점 목록표 표현</li>
+		<li>단계별 풀이/공격 과정을 스냅샷, 공격코드를 포함하여 상세히 서술</li>
+		<li>활용된 공격 코드 첨부가 많을 수록 가산점 부여</li>
+		<li>얼마나 참신한지, 아이디어가 좋은지에 따라 가산점 부여</li>
+		<li>모의해킹 시나리오, ATTACK 등 표현 자유</li>
+	</ul>
+	<br><br>
+	<p class="bg-primary title">666 대상 홈페이지</p>
+	<div class="center">
+		<button type="button" class="btn btn-warning btn-lg" style="background-color : red; "><a href="/shop"
+				style="color : white;" target="_blank">BLACK 마트</a></button>
+		<button type="button" class="btn btn-warning btn-lg" style="background-color : red; "><a href="/shop"
+				style="color : white;" target="_blank">BLACK 마트</a></button>
+		<button type="button" class="btn btn-warning btn-lg" style="background-color : red; "><a href="/shop"
+				style="color : white;" target="_blank">BLACK 마트</a></button>
+		<button type="button" class="btn btn-warning btn-lg" style="background-color : red; "><a href="https://lourcode.kr"
+				style="color : white;" target="_blank">BLACK 마트</a></button>
+	</div>
+	<br><br>
+	</div>
+	</div>
+	<br><br>
+</body>
+
+</html>
